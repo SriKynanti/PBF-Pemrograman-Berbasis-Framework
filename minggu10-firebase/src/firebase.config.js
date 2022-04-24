1583679c6a73cde80e471c1727e14ab30b5f78cf
SriKynanti <kynanti.ti@gmail.com>
2022-04-24 21:42:57 +0700

    Praktikum Jobsheet 10 Firebase

---
+++ minggu10-firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCIqWgJI5rhYubFpkGiPIcqEOrz6kN_Xek",
+    authDomain: "pbf-js-minggu10.firebaseapp.com",
+    projectId: "pbf-js-minggu10",
+    storageBucket: "pbf-js-minggu10.appspot.com",
+    messagingSenderId: "967145073808",
+    appId: "1:967145073808:web:affa21201a224d62197c15",
+    measurementId: "G-N8QVGMGZWK"
+}
