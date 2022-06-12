455f49815a30692f557056d12836417d83e21006
SriKynanti <kynanti.ti@gmail.com>
2022-06-12 21:55:51 +0700

    Perbaikan Kuis

--- minggu13-kuis2/src/App.js
+++ minggu13-kuis2/src/App.js
@@ -1,79 +1,30 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
 import './App.css';
-import firebase from './Firebase';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ListMahasiswa from "./ListMahasiswa";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { AuthProvider } from "./Auth";
+import PrivateRoute from "./PrivateRoute";
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.ref = firebase.firestore().collection('mahasiswas');
-    this.unsubscribe = null;
-    this.state = {
-      mahasiswas: []
-    };
-  }
-
-  onCollectionUpdate = (querySnapshot) => {
-    const mahasiswas = [];
-    querySnapshot.forEach((doc) => {
-      const { nim, nama, alamat, tahunangkatan, status } = doc.data();
-      mahasiswas.push({
-        key: doc.id,
-        doc, // DocumentSnapshot
-        nim,
-        nama,
-        alamat,
-        tahunangkatan,
-        status
-      });
-    });
-    this.setState({
-      mahasiswas
-   });
-  }
-
-  componentDidMount() {
-    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
-  }
-
-  render() {
-    return (
-      <div class="container">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">
-              Mahasiswa LIST
-            </h3>
-          </div>
-          <div class="panel-body">
-            <h4><Link to="/create" class="btn btn-primary">Add Mahasiswa</Link></h4>
-            <table class="table table-stripe">
-              <thead>
-                <tr>
-                  <th>NIM</th>
-                  <th>Nama</th>
-                  <th>Alamat</th>
-                  <th>Tahun Angkatan</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.mahasiswas.map(mahasiswas =>
-                  <tr>
-                    <td><Link to={`/show/${mahasiswas.key}`}>{mahasiswas.nim}</Link></td>
-                    <td>{mahasiswas.nama}</td>
-                    <td>{mahasiswas.alamat}</td>
-                    <td>{mahasiswas.tahunangkatan}</td>
-                    <td>{mahasiswas.status}</td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={ListMahasiswa} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/login" component={Login} />
+          <Route path='/edit/:id' component={Edit} />
+          <Route path='/create' component={Create} />
+          <Route path='/show/:id' component={Show} />
         </div>
-      </div>
-    );
-  }
-}
+      </Router>
+    </AuthProvider>
+  );
+};
 
 export default App;
